b84ced05b3c415c5e66a9acba23afae836215ace
Michael Araujo <michaelaraujoynoa@gmail.com>
2023-02-01 22:44:07 -0400

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Michael Araujo on 2023-02-01.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
