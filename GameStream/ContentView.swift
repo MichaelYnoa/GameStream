90ddaf7f5b05e247b32db0621fc7facd012594de
Michael Araujo <michaelaraujoynoa@gmail.com>
2023-02-01 23:52:21 -0400

    adding: background color, logo, login / signin view

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -9,18 +9,94 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            Spacer()
+            
+            Color(red: 19/255, green: 30/255, blue: 53/255).ignoresSafeArea()
+            
+            VStack{
+                Image("appLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.bottom, 60)
+                
+                InicioYRegistroView()
+            }
+            
         }
-        .padding()
+        
+    }
+}
+
+struct InicioYRegistroView: View {
+   
+    @State var tipoInicioSesion = true
+    
+    var body: some View{
+        
+        VStack{
+            HStack {
+                
+                Spacer()
+                
+                Button("INICIA SESION") {
+                    tipoInicioSesion = true
+                    print("Pantalla Inicio Sesion")
+                }.foregroundColor(tipoInicioSesion ? .white : .gray)
+                
+                Spacer()
+                
+                Button("REGÍSTRATE") {
+                    tipoInicioSesion = false
+                    print("Pantalla de Registro")
+                }.foregroundColor(tipoInicioSesion ? .gray : .white)
+                
+                Spacer()
+                
+            }
+        }
+        
+        Spacer(minLength: 42)
+        
+        if tipoInicioSesion == true {
+            InicioSesionView()
+        }else{
+            RegistroView()
+        }
+        
+        
+        
+    }
+}
+
+struct InicioSesionView:View {
+    var body: some View{
+        
+        VStack{
+            Text("Vista de inicio de sesion")
+        }.foregroundColor(.white)
+        
+        
+    }
+}
+
+struct RegistroView:View {
+    var body: some View{
+        
+        VStack{
+            Text("Vista de inicio de Registro")
+        }.foregroundColor(.white)
+        
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        
         ContentView()
+        
+        Image("pantalla1").resizable()
+        
+        
     }
 }
