978f0d1c7fa52675f595545e2cb29a00f07c1c04
Michael Araujo <michaelaraujoynoa@gmail.com>
2023-02-04 15:44:15 -0400

    signup screen complete

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -28,8 +28,8 @@ struct ContentView: View {
 }
 
 struct InicioYRegistroView: View {
-   
-    @State var tipoInicioSesion = true
+    
+    @State var tipoInicioSesion = false
     
     var body: some View{
         
@@ -119,7 +119,7 @@ struct InicioSesionView:View {
                         .frame(width: 300, alignment: .trailing)
                         .padding(.bottom)
                 }
-
+                
                 
                 
                 
@@ -136,28 +136,27 @@ struct InicioSesionView:View {
                 
                 HStack{
                     
+                    Divider()
+                    
                     Button {
                         print("Iniciar Sesion con Facebook")
                     } label: {
                         Text("Facebook").foregroundColor(.white).bold()
                             .padding(EdgeInsets(top: 5, leading: 20, bottom: 5, trailing: 20))
                             .background(Color(.white).opacity(0.3)).cornerRadius(6)
-                            
                     }
                     
                     Divider()
-
+                    
                     Button {
                         print("Iniciar Sesion con Twitter")
                     } label: {
                         Text("Twitter").foregroundColor(.white).bold()
                             .padding(EdgeInsets(top: 5, leading: 30, bottom: 5, trailing: 30))
                             .background(Color(.white).opacity(0.3)).cornerRadius(6)
-                            
-                            
-                            
-                            
-                    } //35,49,80
+                    }
+                    
+                    Divider()
                     
                 }.frame(maxWidth: .infinity)
                 
@@ -178,23 +177,159 @@ func iniciarSesion() {
 }
 
 struct RegistroView:View {
+    
+    @State var correo = ""
+    @State var contraseña = ""
+    @State var confirmarContraseña = ""
+    
     var body: some View{
         
-        VStack{
-            Text("Vista de inicio de Registro")
-        }.foregroundColor(.white)
         
         
+        ScrollView {
+            
+            VStack(alignment: .center) {
+                
+                Text("Elije una foto de perfil").fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                Text("Puedes cambiar o elegirla mas adelante")
+                    .font(.footnote)
+                    .fontWeight(.light)
+                    .foregroundColor(.gray)
+                    .padding(.bottom)
+                
+                Button(action: tomarFoto, label: {
+                    
+                    ZStack{
+                        Image("foto_perfil")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80, height: 80)
+                        
+                        Image(systemName: "camera")
+                            .foregroundColor(.white)
+                    }
+                })
+            }.padding(.bottom)
+            
+            
+            
+            
+            VStack (alignment: .leading) {
+                
+                VStack(alignment: .leading) {
+                    
+                    Text("Correo Electrónico*").foregroundColor(Color("Dark-cian")).fontWeight(.bold)
+                    
+                    ZStack(alignment: .leading) {
+                        
+                        if correo.isEmpty{
+                            Text("ejemplo @ejemplo.com").font(.caption)
+                                .foregroundColor(.gray)
+                        }
+                        TextField("", text: $correo).foregroundColor(.white)
+                        
+                    }
+                    
+                    Divider().frame(width: 300, height: 1)
+                        .background(Color("Dark-cian"))
+                        .padding(.bottom)
+                    
+                    
+                    Text("Contraseña").foregroundColor(.white).bold()
+                    
+                    ZStack(alignment: .leading) {
+                        
+                        if contraseña.isEmpty{
+                            Text("**********").font(.caption).foregroundColor(.gray)
+                        }
+                        SecureField("", text: $contraseña).foregroundColor(.white)
+                        
+                    }
+                    
+                    Divider().frame(width: 300, height: 1).background(Color("Dark-cian"))
+                    
+                    Text("Confirmar Contraseña").foregroundColor(.white).bold()
+                    
+                    ZStack(alignment: .leading) {
+                        
+                        if contraseña.isEmpty{
+                            Text("**********").font(.caption).foregroundColor(.gray)
+                        }
+                        SecureField("", text: $confirmarContraseña).foregroundColor(.white)
+                        
+                    }
+                    
+                    Divider().frame(width: 300, height: 1).background(Color("Dark-cian"))
+                    
+                }
+                
+                
+                Button(action: registrate) {
+                    Text("REGISTRATE").fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
+                        .overlay(RoundedRectangle(cornerRadius: 6)
+                            .stroke(Color("Dark-cian"), lineWidth: 3).shadow(color: .white, radius: 4))
+                }
+                
+                Text("Registrate con redes sociales").foregroundColor(.white).padding(.top, 50).frame(maxWidth: .infinity, alignment: .center).padding(.bottom)
+                
+                HStack{
+                    
+                    Divider()
+                    
+                    Button {
+                        print("Iniciar Sesion con Facebook")
+                    } label: {
+                        Text("Facebook").foregroundColor(.white).bold()
+                            .padding(EdgeInsets(top: 5, leading: 20, bottom: 5, trailing: 20))
+                            .background(Color(.white).opacity(0.3)).cornerRadius(6)
+                    }
+                    
+                    Divider()
+                    
+                    Button {
+                        print("Iniciar Sesion con Twitter")
+                    } label: {
+                        Text("Twitter").foregroundColor(.white).bold()
+                            .padding(EdgeInsets(top: 5, leading: 30, bottom: 5, trailing: 30))
+                            .background(Color(.white).opacity(0.3)).cornerRadius(6)
+                    }
+                    
+                    Divider()
+                    
+                }.frame(maxWidth: .infinity)
+                
+                
+            }.padding(.horizontal, 77)
+            
+            
+            
+            
+            
+            
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        
-        ContentView()
-        
-        Image("pantalla1").resizable()
-        
-        
+    
+    func tomarFoto() {
+        print("Voy a tomar fotografia de perfil")
+    }
+    
+    func registrate() {
+        print("Te registraste")
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            
+            ContentView()
+            
+            Image("pantalla2").resizable()
+            
+            
+        }
     }
 }
