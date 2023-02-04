a7247ea71beb94613e543ccd29223fe0a069a319
Michael Araujo <michaelaraujoynoa@gmail.com>
2023-02-04 13:57:09 -0400

    add the login button and the facebook and twitter login options

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -81,32 +81,82 @@ struct InicioSesionView:View {
             VStack (alignment: .leading) {
                 
                 
-                Text("Correo Electrónico").foregroundColor(Color("Dark-cian"))
+                Text("Correo Electrónico").foregroundColor(Color("Dark-cian")).fontWeight(.bold)
                 
                 ZStack(alignment: .leading) {
                     
                     if correo.isEmpty{
-                        Text("ejemplo @ gmail.com").font(.caption).foregroundColor(.gray)
+                        Text("ejemplo @ejemplo.com").font(.caption)
+                            .foregroundColor(.gray)
                     }
-                    TextField("", text: $correo)
+                    TextField("", text: $correo).foregroundColor(.white)
                     
                 }
                 
-                Divider().frame(width: 300, height: 1).background(Color("Dark-cian")).padding(.bottom)
+                Divider().frame(width: 300, height: 1)
+                    .background(Color("Dark-cian"))
+                    .padding(.bottom)
                 
                 
-                
+                Text("Contraseña").foregroundColor(.white).bold()
                 
                 ZStack(alignment: .leading) {
                     
                     if contraseña.isEmpty{
                         Text("**********").font(.caption).foregroundColor(.gray)
                     }
-                    SecureField("", text: $contraseña)
+                    SecureField("", text: $contraseña).foregroundColor(.white)
                     
                 }
                 
-                Divider().frame(width: 300, height: 1).background(Color("Dark-cian")).padding(.bottom)
+                Divider().frame(width: 300, height: 1).background(Color("Dark-cian"))
+                
+                Button {
+                    print("Olvidaste tu contraseñ")
+                } label: {
+                    Text("¿Olvidaste tu contraseña?").foregroundColor(Color("Dark-cian"))
+                        .font(.footnote)
+                        .frame(width: 300, alignment: .trailing)
+                        .padding(.bottom)
+                }
+
+                
+                
+                
+                Button(action: iniciarSesion) {
+                    Text("INICIAR SESIÓN").fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
+                        .overlay(RoundedRectangle(cornerRadius: 6)
+                            .stroke(Color("Dark-cian"), lineWidth: 3).shadow(color: .white, radius: 4))
+                }
+                
+                Text("Inicia sesion con redes sociales").foregroundColor(.white).padding(.top, 50).frame(maxWidth: .infinity, alignment: .center).padding(.bottom)
+                
+                HStack{
+                    
+                    Button {
+                        print("Iniciar Sesion con Facebook")
+                    } label: {
+                        Text("Facebook").foregroundColor(.white).bold()
+                            .padding(EdgeInsets(top: 11, leading: 11, bottom: 11, trailing: 11))
+                            .overlay(RoundedRectangle(cornerRadius: 6)
+                            .stroke(Color("Dark-cian"), lineWidth: 1))
+                    }
+                    
+                    Divider()
+
+                    Button {
+                        print("Iniciar Sesion con Twitter")
+                    } label: {
+                        Text("Twitter").foregroundColor(.white).bold()
+                            .padding(EdgeInsets(top: 11, leading: 18, bottom: 11, trailing: 18))
+                            .overlay(RoundedRectangle(cornerRadius: 6)
+                            .stroke(Color("Dark-cian"), lineWidth: 1))
+                    } //35,49,80
+                    
+                }.frame(maxWidth: .infinity)
                 
                 
             }.padding(.horizontal, 77)
@@ -118,6 +168,10 @@ struct InicioSesionView:View {
         
         
     }
+}
+
+func iniciarSesion() {
+    print("estoy iniciando sesion")
 }
 
 struct RegistroView:View {
