f84226ca67faf689d1f125564473980c60f18834
MichaelYnoa <michaelaraujoynoa@gmail.com>
2023-02-21 23:06:24 -0400

    Update color homeScreen and make it the default tab

--- GameStream/Home.swift
+++ GameStream/Home.swift
@@ -8,46 +8,74 @@
 import SwiftUI
 
 struct Home: View {
+    
+    @State var tabSeleccionado: Int = 2
+    
     var body: some View {
         
-        TabView{
+        TabView(selection: $tabSeleccionado){
             
             Text("Perfil")
                 .font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "person")
                     Text("Perfil")
-                }
+                }.tag(0)
             
-            Text("Pantalla de juegos")
+            Text("Pantalla Juegos")
                 .font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "gamecontroller")
                     Text("Juegos")
-                }
+                }.tag(1)
             
             
-            Text("Pantalla Home")
-                .font(.system(size: 30, weight: .bold, design: .rounded))
+            PantallaHome()
                 .tabItem {
                     Image(systemName: "house")
                     Text("Inicio")
-                }
+                }.tag(2)
             
             Text("Pantalla Favoritos")
                 .font(.system(size: 30, weight: .bold, design: .rounded))
                 .tabItem {
                     Image(systemName: "heart")
                     Text("Favoritos")
-                }
+                }.tag(3)
             
             
-        }
+        }.accentColor(.white)
         
         
         Text("Pantalla Home")
     }
+    
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor(Color("TabBarColor"))
+        UITabBar.appearance().isTranslucent = true
+        print("Iniciando las vistas de home")
+    }
 }
+
+
+struct PantallaHome:View {
+    
+    var body: some View{
+        
+        ZStack {
+            
+            Color("marino").ignoresSafeArea()
+            
+            VStack {
+                Text("HOla")
+            }.padding(.horizontal, 18)
+            
+        }.navigationBarHidden(true)
+            .navigationBarBackButtonHidden(true)
+        
+    }
+}
+
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
